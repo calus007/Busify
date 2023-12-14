02bb6aa38aef804f2aee4332227a9ee9af0309da
ShayanTej <50024347+calus007@users.noreply.github.com>
2023-12-14 14:21:05 +0530

    Busify App

--- app/src/main/java/com/cmr/busify/HomeActivity.java
+++ app/src/main/java/com/cmr/busify/HomeActivity.java
@@ -1,14 +1,40 @@
 package com.cmr.busify;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.cmr.busify.databinding.ActivityHomeBinding;
+
 public class HomeActivity extends AppCompatActivity {
+	ActivityHomeBinding binding;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_home);
+		binding = ActivityHomeBinding.inflate(getLayoutInflater());
+		setContentView(binding.getRoot());
+
+		replaceFragment(new HomeFragment());
+		binding.bottomNavigationView.setBackground(null);
+
+		binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+			if (item.getItemId() == R.id.home) {
+				replaceFragment(new HomeFragment());
+			} else if (item.getItemId() == R.id.account) {
+				replaceFragment(new AccountFragment());
+			}
+			return true;
+		});
+	}
+
+	private void replaceFragment(Fragment fragment) {
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+		fragmentTransaction.replace(R.id.frame_layout, fragment);
+		fragmentTransaction.commit();
 	}
 }
