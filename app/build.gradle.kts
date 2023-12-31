d41091ab4411c94f8ee3e7b5fb70ba42afbafb99
ShayanTej <50024347+calus007@users.noreply.github.com>
2023-12-31 22:08:32 +0530

    Busify App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
 	implementation("com.google.zxing:core:3.4.0")
 	implementation("com.journeyapps:zxing-android-embedded:3.6.0")
 	implementation("com.google.android.gms:play-services-wallet:19.3.0-beta01")
+	implementation("com.google.firebase:firebase-firestore:24.10.0")
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/cmr/busify/GetTicketActivity.java
+++ app/src/main/java/com/cmr/busify/GetTicketActivity.java
@@ -1,7 +1,10 @@
 package com.cmr.busify;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -9,16 +12,28 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cmr.busify.databinding.ActivityGetTicketBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Random;
 import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GetTicketActivity extends AppCompatActivity {
+
+	FirebaseFirestore dbf;
+	String uid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+	String ticketNum;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +48,7 @@ public class GetTicketActivity extends AppCompatActivity {
 		TextView validity = binding.validity;
 		TextView costOfTicket = binding.costOfTicket;
 		ImageButton sampleQr = binding.sampleQr;
+		dbf = FirebaseFirestore.getInstance();
 
 		// Retrieve data from the intent
 		Intent intent = getIntent();
@@ -41,44 +57,99 @@ public class GetTicketActivity extends AppCompatActivity {
 			String destination = intent.getStringExtra("end");
 			String fair = intent.getStringExtra("fair");
 
-			ticketNo.setText("Ticket No.:  " + String.valueOf(generateUniqueNumber()));
-			dAT.setText(currentDateAndTime());
+			ticketNum = String.valueOf(generateUniqueNumber());
+			String busNum = busNumber(startingLocation, destination);
+			String validDate = validityDate();
+			String dateAndTime = currentDateAndTime();
+
+			ticketNo.setText("Ticket No.:  " + ticketNum);
+			dAT.setText(dateAndTime);
 			fromAndTo.setText(startingLocation + "   to   " + destination);
-			busNo.setText(busNumber(startingLocation, destination));
-			validity.setText("Valid till Date:  " + validityDate());
+			busNo.setText(busNum);
+			validity.setText("Valid till Date:  " + validDate);
 			costOfTicket.setText(fair);
+
+			Map<String, Object> ticket = new HashMap<>();
+			ticket.put("FROM", startingLocation);
+			ticket.put("TO", destination);
+			ticket.put("TICKET NUMBER", ticketNum);
+			ticket.put("BUS NUMBER", busNum);
+			ticket.put("DATE & TIME", dateAndTime);
+			ticket.put("FAIR", fair);
+			ticket.put("VALID TILL", validDate);
+			ticket.put("isValid", true);
+			ticket.put("didTravel", false);
+
+			dbf.collection(uid)
+					.document(ticketNum)
+					.set(ticket)
+					.addOnSuccessListener(documentReference -> Toast.makeText(GetTicketActivity.this,"Ticket Generated Successfully!", Toast.LENGTH_SHORT).show())
+					.addOnFailureListener(e -> Toast.makeText(GetTicketActivity.this,"Failed! " + e, Toast.LENGTH_SHORT).show());
+
+			scheduleDailyUpdate();
 		}
 
 		sampleQr.setOnClickListener(v -> Toast.makeText(GetTicketActivity.this, "Please make payment to get QR", Toast.LENGTH_SHORT).show());
 	}
 
-	private String calculateTicketData(String startLocation, String destinationLocation) {
-		if (Objects.equals(startLocation, "Suchitra") && Objects.equals(destinationLocation, "CMR College") || Objects.equals(startLocation, "CMR College") && Objects.equals(destinationLocation, "Suchitra"))
-			return "Rs.30.00";
-		else if (Objects.equals(startLocation, "Medchal") && Objects.equals(destinationLocation, "Jubilee Bus Station") || Objects.equals(startLocation, "Jubilee Bus Station") && Objects.equals(destinationLocation, "Medchal"))
-			return "Rs.40.00";
-		else if (Objects.equals(startLocation, "Secunderabad") && Objects.equals(destinationLocation, "Koti") || Objects.equals(startLocation, "Koti") && Objects.equals(destinationLocation, "Secunderabad"))
-			return "Rs.35.00";
-		else if (Objects.equals(startLocation, "Kompally") && Objects.equals(destinationLocation, "Medchal") || Objects.equals(startLocation, "Medchal") && Objects.equals(destinationLocation, "Kompally"))
-			return "Rs.30.00";
-		else if (Objects.equals(startLocation, "Secunderabad") && Objects.equals(destinationLocation, "Patancheruvu") || Objects.equals(startLocation, "Patancheruvu") && Objects.equals(destinationLocation, "Secunderabad"))
-			return "Rs.45.00";
-		else if (Objects.equals(startLocation, "Secunderabad") && Objects.equals(destinationLocation, "LB Nagar") || Objects.equals(startLocation, "LB Nagar") && Objects.equals(destinationLocation, "Secunderabad"))
-			return "Rs.50.00";
-		else if (Objects.equals(startLocation, "Secunderabad") && Objects.equals(destinationLocation, "Hitech City") || Objects.equals(startLocation, "Hitech City") && Objects.equals(destinationLocation, "Secunderabad"))
-			return "Rs.40.00";
-		else if (Objects.equals(startLocation, "Dulapally") && Objects.equals(destinationLocation, "Maisammaguda") || Objects.equals(startLocation, "Maisammaguda") && Objects.equals(destinationLocation, "Dulapally"))
-			return "Rs.30.00";
-		else if (Objects.equals(startLocation, "Dulapally") && Objects.equals(destinationLocation, "Gandimaisamma") || Objects.equals(startLocation, "Gandimaisamma") && Objects.equals(destinationLocation, "Dulapally"))
-			return "Rs.35.00";
-		else if (Objects.equals(startLocation, "Bowenpally") && Objects.equals(destinationLocation, "Miyapur") || Objects.equals(startLocation, "Miyapur") && Objects.equals(destinationLocation, "Bowenpally"))
-			return "Rs.35.00";
-		else {
-			Random random = new Random();
-			int randomNumber = random.nextInt(6) + 4;
-			int randomMultipleOf5 = randomNumber * 5;
-			return "Rs." + randomMultipleOf5 + ".00";
-		}
+	// End of the day ticket validity
+//	private void scheduleDailyUpdate() {
+//		Timer timer = new Timer();
+//		Calendar calendar = Calendar.getInstance();
+//
+//		// Set the time for 12:00 AM
+//		calendar.set(Calendar.HOUR_OF_DAY, 0);
+//		calendar.set(Calendar.MINUTE, 0);
+//		calendar.set(Calendar.SECOND, 0);
+//
+//		// If the current time is already past 12:00 AM, schedule for the next day
+//		if (calendar.getTime().before(new Date())) {
+//			calendar.add(Calendar.DAY_OF_MONTH, 1);
+//		}
+//
+//		// Calculate the delay until the next 12:00 AM
+//		long delay = calendar.getTime().getTime() - new Date().getTime();
+//
+//		// Schedule the task to run daily
+//		timer.scheduleAtFixedRate(new TimerTask() {
+//			@Override
+//			public void run() {
+//				// Update "isValid" field for the ticket
+//				updateTicketValidity();
+//			}
+//		}, delay, 24 * 60 * 60 * 1000); // 24 hours in milliseconds
+//	}
+
+	// 1 min validity for test purpose
+	private void scheduleDailyUpdate() {
+		Timer timer = new Timer();
+		Calendar calendar = Calendar.getInstance();
+
+		// Set the time for 1 minute from now
+		calendar.add(Calendar.MINUTE, 1);
+
+		// Calculate the delay until the next minute
+		long delay = calendar.getTime().getTime() - new Date().getTime();
+
+		// Schedule the task to run every minute
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				// Update "isValid" field for the ticket
+				updateTicketValidity();
+			}
+		}, delay, 60 * 1000); // 1 minute in milliseconds
+	}
+
+	private void updateTicketValidity() {
+		Map<String, Object> updates = new HashMap<>();
+		updates.put("isValid", false);
+
+		dbf.collection(uid)
+				.document(ticketNum)
+				.update(updates)
+				.addOnSuccessListener(aVoid -> Log.d(TAG, "Ticket expired"))
+				.addOnFailureListener(e -> Log.e(TAG, "Failed to update ticket validity", e));
 	}
 
 	private String busNumber(String startLocation, String destinationLocation) {
